7e22897ee97c06108df2e5dcb3868211fdc98654
George W. Walker <george@georgewwalker.com>
2018-12-22 20:55:18 -0600

    Fix analytics

--- js/analytics.js
+++ js/analytics.js
@@ -1,24 +1,12 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
 chrome.storage.sync.get({"disableAnalytics": false}, function(result) {
     if (result.disableAnalytics == false) {
         console.log("Analytics are enabled.");
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        var screenName = "";
-
-        if (document.title) {
-            screenName = document.title;
-        }
-
-        ga('create', 'UA-118042537-2', 'auto');
-        ga('set', 'checkProtocolTask', function(){});
-        ga('send', 'screenview', {
-            'appName': 'Robinhood Mint Sync',
-            'screenName': screenName,
-            'appVersion': chrome.runtime.getManifest().version
-        });
+        sendAnalytics();
     } else {
         console.log("Analytics are disabled.");
         function ga(p1, p2, p3) {
@@ -29,3 +17,27 @@ chrome.storage.sync.get({"disableAnalytics": false}, function(result) {
         }
     }
 })
+
+function sendAnalytics() {
+  if (typeof ga !== "undefined") {
+    console.log("Analytics loaded.");
+    var screenName = "";
+
+    if (document.title) {
+        screenName = document.title;
+    }
+
+    ga('create', 'UA-118042537-2', 'auto');
+    ga('set', 'checkProtocolTask', function(){});
+    ga('send', 'screenview', {
+        'appName': 'Robinhood Mint Sync',
+        'screenName': screenName,
+        'appVersion': chrome.runtime.getManifest().version
+    });
+  } else {
+    setTimeout(function() {
+      sendAnalytics();
+      console.log("Google Analytics not yet loaded.");
+    }, 500);
+  }
+}
