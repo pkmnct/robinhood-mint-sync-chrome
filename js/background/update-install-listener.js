9b79ff53c43150e5ca59a81c639f66425ca1e4fd
George Walker <george@georgewwalker.com>
2019-01-03 12:50:39 -0600

    Replace "plugin" with "extension"

--- js/background/update-install-listener.js
+++ js/background/update-install-listener.js
@@ -1,10 +1,10 @@
 chrome.runtime.onInstalled.addListener(function(details) {
-    // When the plugin is installed
+    // When the extension is installed
     if (details.reason == "install") {
         // Open the welcome page
         window.open(chrome.extension.getURL("/html/welcome.html"), '_blank');
 
-    // When the plugin is updated
+    // When the extension is updated
     } else if (details.reason == "update") {
         // Information about the previous versionCompare
         var previousVersion = details.previousVersion;
