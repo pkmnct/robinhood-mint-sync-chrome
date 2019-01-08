e148b6a485787d2be256551222844a50f7bacf30
George Walker <george@georgewwalker.com>
2019-01-08 16:16:31 -0600

    Improve add functionality

--- js/mint/mint-add.js
+++ js/mint/mint-add.js
@@ -1,41 +1,37 @@
 // First Add
-// https://mint.intuit.com/addprovider.event
-
+// https://mint.intuit.com/addprovider.event?addRobinhood=true
 displayOverlay("Adding Robinhood property to Mint...", "This window will automatically close when complete.");
 
 $(document).ready(function() {
-    function setupRobinhoodProperty() {
-        if (jQuery("#addProperty").length) {
-            console.log("Adding Mint Property...");
-            var accountTotal = "0.00";
+    waitForElement("#addProperty", function() {
+        var accountTotal = "0.00";
+        $("#addProperty").click();
 
-            jQuery("#addProperty").click();
-            jQuery("#addOther").click();
+        waitForElement("#addOther", function() {
+            $("#addOther").click();
 
-            setTimeout(function() {
+            waitForElement(".propertyType", function() {
+                // Choose the option 'Collectible'
                 jQuery(".propertyType").val(jQuery(".propertyType option:contains(Collectible)").val());
-                jQuery("button:contains(Next)").click();
-                setTimeout(function() {
-                    jQuery("#propertyName").val("Robinhood Account")
-                    jQuery("#propertyValue").val(accountTotal);
-                    setTimeout(function() {
-                        jQuery(".addProperty").click();
-                        finishSetup();
-                    }, 10000);
-                }, 10000);
-            }, 10000);
-        } else {
-            setTimeout(setupRobinhoodProperty, 50);
-        }
-    }
-    setupRobinhoodProperty();
 
-    function finishSetup() {
-        if ($(".AddPropertySuccessView").length) {
-            chrome.runtime.sendMessage({"triggerEvent": "addRobinhoodComplete"});
-            window.location = "https://mint.intuit.com/overview.event";
-        } else {
-            setTimeout(finishSetup, 50);
-        }
-    }
-})
+                waitForElement("button:contains(Next)", function() {
+                    jQuery("button:contains(Next)").click();
+
+                    waitForElement("#propertyName", function() {
+                        jQuery("#propertyName").val("Robinhood Account")
+                        jQuery("#propertyValue").val(accountTotal);
+
+                        waitForElement(".addProperty", function() {
+                            jQuery(".addProperty").click();
+
+                            waitForElement(".AddPropertySuccessView", function() {
+                                chrome.runtime.sendMessage({"triggerEvent": "addRobinhoodComplete"});
+                                window.location = "https://mint.intuit.com/overview.event";
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
