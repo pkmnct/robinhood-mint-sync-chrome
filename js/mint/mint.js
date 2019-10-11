002a051f5632232b6b79114ee08b8d3350208135
George W. Walker <george@georgewwalker.com>
2019-10-10 21:22:12 -0500

    Fix issue

--- js/mint/mint.js
+++ js/mint/mint.js
@@ -1,11 +1,5 @@
 chrome.storage.sync.get({"fixTriangle": true}, function(result) {
     if (result.fixTriangle) {
-        $(`
-<style>
-.rhmsc-fix-triangle .disclosure-triangle {
-    transform: rotate(0deg);
-}
-</style>
-`)
+        $("body").addClass("rhmsc-fix-triangle");
     }
 });
