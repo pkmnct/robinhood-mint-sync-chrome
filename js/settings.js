38716397111b83772e4ab9e2597bd201c86eb377
George W. Walker <george@georgewwalker.com>
2019-10-10 21:39:15 -0500

    Triangle Indicator Fix (#7)
    In some areas of Mint, there is an expansion panel where the indicator functions counterintuitively. This setting enables a fix so that the indicator for the panel works like most other websites/UIs.

--- js/settings.js
+++ js/settings.js
@@ -1,10 +1,13 @@
-chrome.storage.sync.get({"changelogOnUpdate": true, "disableAnalytics": true}, function(result) {
+chrome.storage.sync.get({"changelogOnUpdate": true, "disableAnalytics": true, "fixTriangle": true}, function(result) {
     console.log(result);
     if (result.changelogOnUpdate) {
         $("#setting-changelogOnUpdate").prop("checked", true);
     }
     if (result.disableAnalytics) {
         $("#setting-disableAnalytics").prop("checked", true);
+    }
+    if (result.fixTriangle) {
+        $("#setting-fixTriangle").prop("checked", true);
     }
     $("#setting-changelogOnUpdate").change(function() {
         chrome.storage.sync.set({"changelogOnUpdate":  $("#setting-changelogOnUpdate").is(':checked')});
@@ -13,5 +16,8 @@ chrome.storage.sync.get({"changelogOnUpdate": true, "disableAnalytics": true}, f
         console.log("change");
         console.log($("#setting-disableAnalytics").is(':checked'));
         chrome.storage.sync.set({"disableAnalytics":  $("#setting-disableAnalytics").is(':checked')});
+    });
+    $("#setting-fixTriangle").change(function() {
+        chrome.storage.sync.set({"fixTriangle":  $("#setting-fixTriangle").is(':checked')});
     });
 });
