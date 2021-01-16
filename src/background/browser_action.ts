60305c81cdc0313bdfe5b1b32c07e1856de2ba9c
George W. Walker <george@georgewwalker.com>
2021-01-15 19:55:06 -0500

    Fix HTML rendering

--- src/background/browser_action.ts
+++ src/background/browser_action.ts
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.create({
-    url: chrome.extension.getURL("/pages/welcome/"),
+    url: chrome.extension.getURL("html/welcome.html"),
     active: true,
     openerTabId: tab.id,
   });
--- src/background/update_install_listener.ts
+++ src/background/update_install_listener.ts
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === "install") {
     // When the extension is installed
-    window.open(chrome.extension.getURL("pages/welcome/"), "_blank");
+    window.open(chrome.extension.getURL("html/welcome.html"), "_blank");
   } else if (details.reason === "update") {
     // When the extension is updated
     // TODO: set up a notification to show that the extension was updated, with a link to changelog
