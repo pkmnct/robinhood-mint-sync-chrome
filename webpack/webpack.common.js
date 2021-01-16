5a1c1f6a78388a5af3a6b5d3e269d37926a443c9
George W. Walker <george@georgewwalker.com>
2021-01-16 13:32:37 -0500

    Manually resolve noty for now

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,47 +1,49 @@
 const webpack = require("webpack");
-const path = require('path');
-const glob = require('glob')
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const glob = require("glob");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const entryPoints = {};
 
-glob.sync("./src/**/*.ts").forEach(item => {
-    const key = item.replace("./src/","").replace(".ts", "");
-    entryPoints[key] = item;
+glob.sync("./src/**/*.ts").forEach((item) => {
+  const key = item.replace("./src/", "").replace(".ts", "");
+  entryPoints[key] = item;
 });
 
 module.exports = {
-    entry: entryPoints,
-    output: {
-        filename: '[name].js',
-        path: path.join(__dirname, '../dist/js')
+  entry: entryPoints,
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "../dist/js"),
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "initial",
     },
-    optimization: {
-        splitChunks: {
-            name: 'vendor',
-            chunks: "initial"
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    plugins: [
-        // exclude locale files in moment
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new CopyPlugin({
-            patterns: [
-                { from: '.', to: '../', context: 'public' },
-                { from: 'node_modules/jquery/dist', to: 'external/jquery' }
-            ]
-        }),
-    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+    modules: ["node_modules"],
+  },
+  plugins: [
+    // exclude locale files in moment
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new CopyPlugin({
+      patterns: [
+        { from: ".", to: "../", context: "public" },
+        { from: "node_modules/jquery/dist", to: "external/jquery" },
+        { from: "node_modules/noty/lib", to: "external/noty" },
+      ],
+    }),
+  ],
 };
