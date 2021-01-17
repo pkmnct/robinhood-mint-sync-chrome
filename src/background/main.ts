c66023009ab2feafe52b0d272cb53ff5720a092c
George W. Walker <george@georgewwalker.com>
2021-01-17 02:58:48 -0500

    Use mintTab as opener, ensure undefined tab check runs before tab access (fix loop)

--- src/background/main.ts
+++ src/background/main.ts
@@ -4,7 +4,7 @@ import { Debug } from "../utilities/debug";
 const debug = new Debug("background", "Main");
 
 // Need to be able to access this regardless of the message.
-let mintTab;
+let mintTab: undefined | number;
 
 interface eventHandler {
   message: any;
@@ -15,7 +15,7 @@ interface eventHandler {
 const eventHandlers = {
   // This event is emitted by the main Robinhood content script.
   "robinhood-login-needed": ({ sender }: eventHandler) => {
-   debug.log("robinhood-login-needed event");
+    debug.log("robinhood-login-needed event");
     chrome.tabs.sendMessage(mintTab, {
       status: "You need to log in to Robinhood",
       link: urls.robinhood.login,
@@ -27,7 +27,7 @@ const eventHandlers = {
   },
   // This event is emitted by the login Robinhood content script.
   "robinhood-login-success": ({ sender }: eventHandler) => {
-   debug.log("robinhood-login-success event");
+    debug.log("robinhood-login-success event");
 
     // Close the Robinhood tab logged in from
     if (!debug.isEnabled()) chrome.tabs.remove(sender.tab.id);
@@ -46,29 +46,30 @@ const eventHandlers = {
   },
   // This event is emitted by the main Robinhood content script.
   "robinhood-portfolio-scraped": ({ sender, message }: eventHandler) => {
-   debug.log("robinhood-portfolio-scraped event");
+    debug.log("robinhood-portfolio-scraped event");
     // Trigger the Mint portfolio update content script
     chrome.tabs.create(
       {
         url: urls.mint.properties.update,
         active: false,
-        openerTabId: sender.tab.id,
+        openerTabId: mintTab,
       },
       (tab) => {
         debug.log("waiting for Mint tab to load")
         const checkIfLoaded = () => {
-          chrome.tabs.get(tab.id, (tab) => {
-            if (!tab) {
-              clearInterval(sendMessageInterval);
-              debug.log("tab was not found. Clearing interval to prevent endless loop.");
-            }
-            if (tab.status === "complete") {
-              // Once the tab is loaded, pass the message to it
-              chrome.tabs.sendMessage(tab.id, message);
-              clearInterval(sendMessageInterval);
-              debug.log("Mint tab loaded");
-            }
-          });
+          if (!tab) {
+            clearInterval(sendMessageInterval);
+            debug.log("Unexpected: Tab was not found. Clearing interval to prevent endless loop. Did the tab get closed?");
+          } else {
+            chrome.tabs.get(tab.id, (tab) => {
+              if (tab.status === "complete") {
+                // Once the tab is loaded, pass the message to it
+                chrome.tabs.sendMessage(tab.id, message);
+                clearInterval(sendMessageInterval);
+                debug.log("Mint tab loaded");
+              }
+            });
+          }
         };
         const sendMessageInterval = setInterval(checkIfLoaded, 200);
       }
@@ -77,7 +78,7 @@ const eventHandlers = {
   },
   // This event is emitted by the Mint main content script.
   "mint-force-sync": () => {
-   debug.log("mint-force-sync event");
+    debug.log("mint-force-sync event");
     // Trigger the main Robinhood sync script
     chrome.tabs.create({
       url: urls.robinhood.scrape,
@@ -87,12 +88,12 @@ const eventHandlers = {
   },
   // This event is emitted by the Mint property create content script.
   "mint-property-added": ({ sender }: eventHandler) => {
-   debug.log("mint-property-added event");
-   if (!debug.isEnabled()) chrome.tabs.remove(sender.tab.id);
+    debug.log("mint-property-added event");
+    if (!debug.isEnabled()) chrome.tabs.remove(sender.tab.id);
   },
   // This event is emitted by the Mint property check content script.
   "mint-property-setup-complete": ({ sender }: eventHandler) => {
-   debug.log("mint-property-setup-complete event");
+    debug.log("mint-property-setup-complete event");
 
     chrome.storage.sync.set({
       propertiesSetup: true,
@@ -119,7 +120,7 @@ const eventHandlers = {
   },
   // This event is emitted by the Mint property update content script.
   "mint-sync-complete": () => {
-   debug.log("mint-sync-complete event");
+    debug.log("mint-sync-complete event");
     chrome.storage.sync.set({ syncTime: new Date().toString() });
     chrome.tabs.sendMessage(mintTab, {
       status: "Sync Complete! Reload to see the change.",
@@ -130,7 +131,7 @@ const eventHandlers = {
   },
   // This event is emitted by the main Mint content script
   "mint-opened": ({ sender }: eventHandler) => {
-   debug.log("mint-opened event");
+    debug.log("mint-opened event");
     // Store a reference to the mint tab to be able to show the notifications
     mintTab = sender.tab.id;
 
@@ -197,16 +198,16 @@ const eventHandlers = {
     );
   },
   // This event is emitted by the Mint property check content script.
-  "mint-create": ({ message, sender }: eventHandler) => {
-   debug.log("mint-create event");
+  "mint-create": ({ message }: eventHandler) => {
+    debug.log("mint-create event");
     chrome.tabs.create({
       url: urls.mint.properties.create + "&property=" + message.property,
       active: false,
     });
   },
   // This event is emitted by the Mint property check content script.
-  "mint-property-remove": ({ message, sender }: eventHandler) => {
-   debug.log("mint-property-remove event");
+  "mint-property-remove": ({ sender }: eventHandler) => {
+    debug.log("mint-property-remove event");
     chrome.tabs.sendMessage(mintTab, {
       status:
         "Your account was set up prior to version 3 of this extension. Version 3 introduced separation of asset types when syncing. Please remove the old 'Robinhood Account' property from Mint to prevent duplication of your portfolio balance. Reload the overview to sync after removing the property.",
