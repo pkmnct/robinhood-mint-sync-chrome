391686d18b2c2a7ca55e21e970339f07243ba753
George W. Walker <george@georgewwalker.com>
2021-01-17 13:15:25 -0500

    Add (optional) initialcontainer parameter

--- src/utilities/waitForElement.ts
+++ src/utilities/waitForElement.ts
@@ -1,25 +1,29 @@
 export const waitForElement = (
   selector: string,
   withText: string | null,
-  callback: () => void
+  callback: (foundElement?: HTMLElement) => void,
+  initialContainer?: Element
 ): void => {
-  const elements = document.querySelectorAll(selector);
+  const queryContainer = initialContainer ? initialContainer : document;
+  const elements = queryContainer.querySelectorAll(selector);
   if (!elements.length) {
-    setTimeout(() => waitForElement(selector, withText, callback), 500);
+    setTimeout(() => waitForElement(selector, withText, callback, initialContainer), 500);
   } else if (withText) {
     let foundText = false;
+    let foundElement;
     elements.forEach((element) => {
-      if ((element as HTMLElement).innerText.toLowerCase().includes(withText)) {
+      if ((element as HTMLElement).innerText.toLowerCase().includes(withText.toLowerCase())) {
         foundText = true;
+        foundElement = element;
         return;
       }
     });
     if (foundText) {
-      callback();
+      callback(foundElement);
     } else {
-      setTimeout(() => waitForElement(selector, withText, callback), 500);
+      setTimeout(() => waitForElement(selector, withText, callback, initialContainer), 500);
     }
   } else {
-    callback();
+    callback(elements[0] as HTMLElement);
   }
 };
