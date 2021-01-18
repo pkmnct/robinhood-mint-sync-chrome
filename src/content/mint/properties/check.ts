65d5d9270e0ff348bb95fc1ae2dfb19410cdf4fd
George W. Walker <george@georgewwalker.com>
2021-01-18 05:03:20 -0500

    Add comments, small fixes, rename failureCallback to onError

--- src/content/mint/properties/check.ts
+++ src/content/mint/properties/check.ts
@@ -9,24 +9,31 @@ const debug = new Debug("content", "Mint - Properties - Check");
 
 new Overlay("Checking if Robinhood account set up in Mint...", "This window will automatically close when complete.");
 
+// The properties to set up/check for
 export const robinhoodProperties = ["Cash", "Stocks", "Crypto", "Other"];
 
+// Function to check if the propert exists on the page
 const checkIfPropertyExists = (property) => {
   debug.log("Checking if property exists:", property);
+
   const propertyElements = document.querySelectorAll(".OtherPropertyView");
+
   let foundProperty = false;
-  propertyElements.forEach((propertyElement) => {
+  for (const propertyElement of propertyElements) {
     if ((propertyElement as HTMLElement).innerText.includes(`Robinhood ${property}`)) {
       debug.log(`Found property "${property}"`, propertyElement);
       foundProperty = true;
+      // Stop searching
+      break;
     }
-  });
+  }
   return foundProperty;
 };
 
-const checkForCreateProperties = () => {
-  debug.log("Found Property View or Zero State.");
+const setupProperties = () => {
+  // Need to keep track of how many properties we added to ensure each gets set up before initiating sync
   let newProperties = 0;
+
   robinhoodProperties.forEach((property) => {
     if (!checkIfPropertyExists(property)) {
       debug.log(`Could not find "${property}", adding it.`);
@@ -60,12 +67,21 @@ window.addEventListener("load", () => {
   debug.log("Waiting for .OtherPropertyView");
   waitForElement({
     selector: ".OtherPropertyView",
-    callback: checkForCreateProperties,
-    failureCallback: () => {
-      debug.log('Failed to find "Other" Property View. Checking for Zero State');
+    failureAttempts: 20,
+    callback: (result) => {
+      debug.log("Found Property View. Setting up properties.", result);
+      setupProperties();
+    },
+    onError: () => {
+      // If we don't find the OtherPropertyView, check if no properties are set up
+      debug.log("Did not find property view. Checking for zeroState");
       waitForElement({
         selector: ".zeroState",
-        callback: checkForCreateProperties,
+        callback: () => {
+          setupProperties();
+        },
+        onError: (error) => debug.error("Did not find zero state", error),
+        failureAttempts: 1, // We should fail right away if it doesn't exist. It's already waited many seconds for OtherPropertyView
       });
     },
   });
