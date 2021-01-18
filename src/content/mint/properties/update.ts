4360e13a9d76b2f19715921411f93f07ebb2bcbb
George W. Walker <george@georgewwalker.com>
2021-01-18 00:33:27 -0500

    Improve Prettier, ESLint setup (#29)
    Maintenance:
    - Add scripts to run Prettier, ESLint manually via npm
    - Add VS Code configuration to run ESLint on save
    - Add Husky, Lint-Staged to run Prettier and ESLint on commit
    - Add GitHub Actions to check build, code standards on Pull Requests
    - Add additional debugging
    - Minor enhancements

--- src/content/mint/properties/update.ts
+++ src/content/mint/properties/update.ts
@@ -9,7 +9,7 @@ const debug = new Debug("content", "Mint - Properties - Update");
 
 new Overlay("Updating Mint Properties...", "This window will automatically close when the sync is complete");
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((request) => {
   if (request.event === "robinhood-portfolio-scraped") {
     debug.log("Waiting for Property Tab View to load");
     waitForElement(".PropertyTabView", null, (propertyViewElement) => {
@@ -21,14 +21,14 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       let other = 0;
       const syncedLabels = [];
 
-      function closeWindow() {
+      const closeWindow = () => {
         if (document.querySelectorAll(".AccountView.open").length) {
           setTimeout(closeWindow, 50);
         } else {
           chrome.runtime.sendMessage({ event: "mint-sync-complete" });
           if (!debug.isEnabled()) window.close();
         }
-      }
+      };
 
       const callback = () => {
         if (syncedLabels.length === 4) {
@@ -43,7 +43,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         }
       };
 
-      function setRobinhoodAmount(label, amount) {
+      const setRobinhoodAmount = (label, amount) => {
         debug.log(`Attempting to set ${label} to ${amount}`);
         waitForElement(".OtherPropertyView", `Robinhood ${label}`, (foundElement) => {
           debug.log(`Expanding property ${label}`, foundElement);
@@ -66,7 +66,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             foundElement
           );
         });
-      }
+      };
 
       if (request.uninvested_cash) {
         cash = parseFloat(request.uninvested_cash);
