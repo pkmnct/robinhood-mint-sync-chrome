dbe258bce673e7994cb620f4782978138a9efce2
George W. Walker <george@georgewwalker.com>
2021-01-18 18:49:56 -0500

    Add docs to main repo

---
+++ docs/js/analytics.js
@@ -0,0 +1,7 @@
+jQuery.getScript("https://www.googletagmanager.com/gtag/js?id=UA-118042537-2", function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-118042537-2');
+});
