2564a9c841a39251b9f706d63f74f0f6123d516d
Andrew Herder <andrew.herder@gmail.com>
2021-01-18 04:15:42 -0600

    Improve waitForElement utility, fix property setup when no properties present (#27)
    * Changes waitForElement to go to a failure state after 5 seconds && Allows for a failure call back.
    
    Allows properties to be added from a "Zero State" (No other properties on the account)
    
    Fixes the button clicking on create, which can sometimes click the wrong buttons.
    
    * Move all object args to newlines
    
    * Remove more useless of the logs
    
    * Small cleanup
    
    * Refactor/clean up waitForElement
    
    * Add console.error support to debug
    
    * Add comments, small fixes, rename failureCallback to onError
    
    * Make onError required
    
    * Bump version
    
    Co-authored-by: George W. Walker <george@georgewwalker.com>

--- src/content/mint/properties/check.ts
+++ src/content/mint/properties/check.ts
@@ -9,47 +9,80 @@ const debug = new Debug("content", "Mint - Properties - Check");
 
 new Overlay("Checking if Robinhood account set up in Mint...", "This window will automatically close when complete.");
 
+// The properties to set up/check for
 export const robinhoodProperties = ["Cash", "Stocks", "Crypto", "Other"];
 
+// Function to check if the propert exists on the page
 const checkIfPropertyExists = (property) => {
   debug.log("Checking if property exists:", property);
+
   const propertyElements = document.querySelectorAll(".OtherPropertyView");
+
   let foundProperty = false;
-  propertyElements.forEach((propertyElement) => {
+  for (const propertyElement of propertyElements) {
     if ((propertyElement as HTMLElement).innerText.includes(`Robinhood ${property}`)) {
       debug.log(`Found property "${property}"`, propertyElement);
       foundProperty = true;
+      // Stop searching
+      break;
+    }
+  }
+  return foundProperty;
+};
+
+const setupProperties = () => {
+  // Need to keep track of how many properties we added to ensure each gets set up before initiating sync
+  let newProperties = 0;
+
+  robinhoodProperties.forEach((property) => {
+    if (!checkIfPropertyExists(property)) {
+      debug.log(`Could not find "${property}", adding it.`);
+      // Trigger setup of property
+      chrome.runtime.sendMessage({
+        event: "mint-create",
+        property,
+      });
+      newProperties++;
     }
   });
-  return foundProperty;
+
+  // Check for old version
+  if (checkIfPropertyExists("Account")) {
+    debug.log("Found old Robinhood Account property. This must be removed to finish setup.");
+    chrome.runtime.sendMessage({
+      event: "mint-property-remove",
+    });
+    return;
+  }
+
+  // Success.
+  debug.log("Finishing Setup");
+  chrome.runtime.sendMessage({
+    event: "mint-property-setup-complete",
+    newProperties,
+  });
 };
 
 window.addEventListener("load", () => {
   debug.log("Waiting for .OtherPropertyView");
-  waitForElement(".OtherPropertyView", null, () => {
-    let newProperties = 0;
-    robinhoodProperties.forEach((property) => {
-      if (!checkIfPropertyExists(property)) {
-        debug.log(`Could not find "${property}", adding it.`);
-        // Trigger setup of property
-        chrome.runtime.sendMessage({
-          event: "mint-create",
-          property,
-        });
-        newProperties++;
-      }
-    });
-    if (checkIfPropertyExists("Account")) {
-      debug.log("Found old Robinhood Account property. This must be removed to finish setup.");
-      chrome.runtime.sendMessage({
-        event: "mint-property-remove",
+  waitForElement({
+    selector: ".OtherPropertyView",
+    failureAttempts: 20,
+    callback: (result) => {
+      debug.log("Found Property View. Setting up properties.", result);
+      setupProperties();
+    },
+    onError: () => {
+      // If we don't find the OtherPropertyView, check if no properties are set up
+      debug.log("Did not find property view. Checking for zeroState");
+      waitForElement({
+        selector: ".zeroState",
+        callback: () => {
+          setupProperties();
+        },
+        onError: (error) => debug.error("Did not find zero state", error),
+        failureAttempts: 1, // We should fail right away if it doesn't exist. It's already waited many seconds for OtherPropertyView
       });
-    } else {
-      debug.log("Finishing Setup");
-      chrome.runtime.sendMessage({
-        event: "mint-property-setup-complete",
-        newProperties,
-      });
-    }
+    },
   });
 });
